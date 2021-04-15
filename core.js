7cf5694376b039311d8e6252d94874ef9b6d0c5e
Stefan <seje26597@edu.ucl.dk>
2021-04-15 17:24:54 +0200

    Content
    Jeg fik lavet overlay til emnerne omkring ensomhed

--- core.js
+++ core.js
@@ -1,5 +1,5 @@
 console.log('core.js is working!');
-
+//Slide
 var slideCount = 0;
 
 
@@ -22,4 +22,24 @@ if (slideCount > slides.length) {slideCount = 1}
 
 slides[slideCount-1].style.display = "block";
 setTimeout(showSlides, 12000);
+}
+
+//Overlay
+//The code is inspired by w3school; https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup_chat
+function openFormEnsom() {
+    document.getElementById("myFormEnsom").style.display = "block";
+    //document.getElementById("background-opacity").style.display = "block";
+}
+
+function closeFormEnsom() {
+    document.getElementById("myFormEnsom").style.display = "none";
+    //document.getElementById("background-opacity").style.display = "none";
+}
+// **********************************************************************************************************************
+function openFormSygdom() {
+    document.getElementById("myFormSygdom").style.display = "block";
+}
+
+function closeFormSygdom() {
+    document.getElementById("myFormSygdom").style.display = "none";
 }
